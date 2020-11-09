1758885d301ff896950395f80079e9e38bb12f8a
edumoreira1506 <eduardo.moreira@codeminer42.com>
2020-11-09 17:19:18 -0300

    Add 0.3 version

--- app/models/utfpr_gp_engine/student.rb
+++ app/models/utfpr_gp_engine/student.rb
@@ -1,7 +1,5 @@
 module UtfprGpEngine
   class Student < ApplicationRecord
-    attr_acessible :name
-
     validates :name, presence: true
   end
 end
--- lib/utfpr_gp_engine/version.rb
+++ lib/utfpr_gp_engine/version.rb
@@ -1,3 +1,3 @@
 module UtfprGpEngine
-  VERSION = '0.2.0'
+  VERSION = '0.3.0'
 end
