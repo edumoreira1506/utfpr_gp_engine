85efede048485adff4688a980cae7fd49e064b29
edumoreira1506 <eduardo.moreira@codeminer42.com>
2020-11-09 18:01:40 -0300

    Add version 0.5

--- lib/utfpr_gp_engine/engine.rb
+++ lib/utfpr_gp_engine/engine.rb
@@ -1,5 +1,11 @@
 module UtfprGpEngine
   class Engine < ::Rails::Engine
     isolate_namespace UtfprGpEngine
+
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/overrides/**/*_override*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
--- lib/utfpr_gp_engine/version.rb
+++ lib/utfpr_gp_engine/version.rb
@@ -1,3 +1,3 @@
 module UtfprGpEngine
-  VERSION = '0.4.0'
+  VERSION = '0.5.0'
 end
